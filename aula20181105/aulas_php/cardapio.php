f6a8882433a67d63a705066f3e340fdd530924b9
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-05 21:37:56 -0200

    Utilizando Switch

---
+++ aula20181105/aulas_php/cardapio.php
@@ -0,0 +1,26 @@
+<?php
+
+    $numero = 2;
+
+    switch($numero){
+        case(1):
+            echo " Você selecionou Bife a Cavalo";
+        break;
+        case(2):
+            echo " Você selecionou Filé de Frango Grelhado";
+        break;
+        case(3):
+            echo " Você selecionou Filé mignon à Parmegiana";
+        break;
+        case(4):
+            echo " Você selecionou Filé de Frango à Parmegiana";
+        break;
+        case(5):
+            echo " Você selecionou Risoto Vegano";
+        break;
+        case(6):
+            echo " Você selecionou Massa Tradicional";
+        break;
+
+        
+    }
