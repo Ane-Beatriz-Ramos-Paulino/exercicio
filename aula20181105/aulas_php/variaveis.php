57cb1f05565a231d40526fdf526e04c6898deadf
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-05 19:37:00 -0200

    Utilizando variáveis para salvar dados pessoais

---
+++ aula20181105/aulas_php/variaveis.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nome = "Ane Beatriz";
+    $sobrenome = "Ramos Paulino";
+    $endereco = " Rua dos bobos, 0 - Jardim das flores - Barueri-SP";
+    $idade = 21 ;
+    $telefone = "(11) 95368-6924";
+
+    echo "Nome: " .$nome.$sobrenome."<br>";
+    echo "Endereço: ".$endereco."<br>";
+    echo "Idade: ".$idade."<br>";
+    echo "Telefone: ".$telefone."<br>";
+
+ ?>   
+    
