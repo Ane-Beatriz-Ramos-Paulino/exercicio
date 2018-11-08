67e2ba8c7f32f89f24d8f54851733ba5caee7430
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-08 12:41:50 -0200

    Exercício I

---
+++ aula20181105/aulas_php/ExercicioI.php
@@ -0,0 +1,14 @@
+<?php
+
+        $var1 = 10;
+        $var2 = 10;
+        $soma = $var1 + $var2;
+
+
+    if($soma>20){
+        echo ($soma + 8);
+    }else{
+        echo ($soma-5);
+    }
+    
+?>    
