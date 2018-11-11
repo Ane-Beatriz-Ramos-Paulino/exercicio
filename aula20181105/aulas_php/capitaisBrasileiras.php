3182fde51b4199aa80a159701d07bb312b119998
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 17:11:56 -0200

    CAPITAIS BRASILEIRAS

---
+++ aula20181105/aulas_php/capitaisBrasileiras.php
@@ -0,0 +1,26 @@
+<?php
+
+    $capitais = array (
+        "Florianópolis",
+        "São Paulo",
+        "Rio de Janeiro",
+        "Curitiba",
+        "Palmas",
+        "Porto Alegre",
+        "Natal",
+        "Rio Branco",
+        "Recife",
+        "Belém"
+
+    );
+
+    echo current ($capitais) . "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
+    echo next ($capitais). "<br>";
