80d37fb76132aef6a265e3b6b06bcb4427971077
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 16:26:28 -0200

    Personalidades Históricas

---
+++ aula20181105/aulas_php/personalidadesHistoricas.php
@@ -0,0 +1,28 @@
+<?php
+
+$personalidades = array (
+    "Isaac Newton",
+    "Chiquinha Gonzaga",
+    "Cleópatra VII",
+    "Joana d’Arc",
+    "Maria Quitéria",
+    "Anita Garibaldi",
+    "Cora Coralina",
+    "Carmen Miranda",
+    "Teresa de Calcutá",
+    "Indira Gandhi",
+    "Marilyn Monroe",
+    "Benazir Bhutto",
+    "Diana de Gales",
+    "William Shakespeare",
+    "Henry Ford",
+    "Homero",
+    "Sigmund Freud",
+    "Confúcio",
+    "Aristóteles",
+    "Sócrates"
+);
+
+foreach($personalidades as $personalidade){
+    echo "$personalidade <br>";
+}
