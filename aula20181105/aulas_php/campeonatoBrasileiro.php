dc00541b9e12ee945b72265c4b06d8722eba0df0
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 16:00:00 -0200

    Campeonato Brasileiro

---
+++ aula20181105/aulas_php/campeonatoBrasileiro.php
@@ -0,0 +1,28 @@
+<?php
+
+$times = array (
+    "Palmeiras",
+    "Internacional", 
+    "Flamengo",
+    "São Paulo",
+    "Grêmio",
+    "Atlético Paranaense",
+    "Atlético Mineiro",
+    "Santos",
+    "Cruzeiro",
+    "Botafogo",
+    "Fluminense",
+    "Corinthians",
+    "Bahia",
+    "Vasco da Gama",
+    "Ceará",
+    "Sport",
+    "Vitória",
+    "América Mineiro",
+    "Chapecoense",
+    "Paraná"
+);
+
+foreach($times as $time){
+    echo $time . "<br>";
+}
