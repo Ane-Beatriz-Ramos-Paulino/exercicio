22b2e78b9fc855d1cdc8295b7da05462f453c0d2
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 18:39:41 -0200

    Dados da cidade de São Paulo

---
+++ aula20181105/aulas_php/cidadeSaoPaulo.php
@@ -0,0 +1,13 @@
+<?php
+
+    $saoPaulo = array (
+        "aniversario"=>"25 de Janeiro",
+        "fundacao"=> "25 de janeiro de 1554",
+        "gentilico"=>"Paulistano",
+        "lema"=>"Não sou conduzido, conduzo",
+        "padroeiro"=>"São Paulo",
+        "prefeito"=>"Bruno Covas Lopes"
+    );
+
+    print("<pre>");
+    var_dump($saoPaulo);
