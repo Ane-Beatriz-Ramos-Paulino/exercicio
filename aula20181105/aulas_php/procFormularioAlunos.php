2c69080be990316ceccf46d88b18583e85d84d8b
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 21:46:25 -0200

    Formulario Alunos (nome, período, matérias)

--- aula20181105/aulas_php/procFormularioAlunos.php
+++ aula20181105/aulas_php/procFormularioAlunos.php
@@ -2,4 +2,11 @@
 
     echo "Os dados enviados foram: <br><br>";
     echo "Nome: ". $_POST["nome"] . "<br>";
-    echo "Período: ". $_POST["periodo"] . "<hr>";
+    echo "Período: ". $_POST["periodo"] . "<br>";
+    echo "Matérias: ";
+
+    foreach($_POST[materias] as $materia){
+        echo  $materia . ", ";
+    }
+
+    echo "<hr>";
