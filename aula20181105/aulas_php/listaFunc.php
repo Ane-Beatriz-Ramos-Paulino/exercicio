e109f05f64576ada31b98759b8efc7d339d356e6
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 14:30:53 -0200

    Lista de funcionários

---
+++ aula20181105/aulas_php/listaFunc.php
@@ -0,0 +1,16 @@
+<?php
+
+    $func = array("Ane", "Jorge", "Pedro", "Franciele", "Manassés" , "André", "Danilo", "Nathaly", "Brunna", "Amanda", "Dulce");
+
+    echo $func[0] ."<br>";
+    echo $func[1] ."<br>";
+    echo $func[2] ."<br>";
+    echo $func[3] ."<br>";
+    echo $func[4] ."<br>";
+    echo $func[5] ."<br>";
+    echo $func[6] ."<br>";
+    echo $func[7] ."<br>";
+    echo $func[8] ."<br>";
+    echo $func[9] ."<br>";
+    echo $func[10] ."<br>";
+    
