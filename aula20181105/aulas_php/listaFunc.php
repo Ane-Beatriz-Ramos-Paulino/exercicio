783fc9ecfd96868adada7b4cc56538556d880a4b
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 15:22:06 -0200

    Lista de funcionários com DO WHILE

--- aula20181105/aulas_php/listaFunc.php
+++ aula20181105/aulas_php/listaFunc.php
@@ -1,16 +1,23 @@
 <?php
 
-    $func = array("Ane", "Jorge", "Pedro", "Franciele", "Manassés" , "André", "Danilo", "Nathaly", "Brunna", "Amanda", "Dulce");
+    $contador = 0;
+    $func = array(
+        "Ane",
+         "Jorge", 
+         "Pedro", 
+         "Franciele",
+         "Manassés" , 
+         "André", 
+         "Danilo", 
+         "Nathaly", 
+         "Brunna", 
+         "Amanda", 
+         "Dulce"
+        );
 
-    echo $func[0] ."<br>";
-    echo $func[1] ."<br>";
-    echo $func[2] ."<br>";
-    echo $func[3] ."<br>";
-    echo $func[4] ."<br>";
-    echo $func[5] ."<br>";
-    echo $func[6] ."<br>";
-    echo $func[7] ."<br>";
-    echo $func[8] ."<br>";
-    echo $func[9] ."<br>";
-    echo $func[10] ."<br>";
-    
+        do {
+            echo $contador . " - " . $func[$contador] . "<br>";
+            $contador++;
+        }while($contador < count($func));
+
+  
