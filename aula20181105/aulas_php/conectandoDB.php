d4cab3bd804a6fcffd7543772ccc1cb4a422ffc1
Ane_Beatriz_Ramos_Paulino <ane.b2010@gmail.com>
2018-11-11 23:58:15 -0200

    Recuperando dados DB

--- aula20181105/aulas_php/conectandoDB.php
+++ aula20181105/aulas_php/conectandoDB.php
@@ -3,12 +3,23 @@
     $servidor = "cursophp_db_1";
     $usuario = "root";
     $senha = "phprs";
+    $banco = "curso_php";
 
     $conn = new mysqli($servidor, $usuario, $senha);
 
     if ($conn->conect_error){
         die("Erro de conexão" . $conn->conect_error);
 
-    }else{
-        echo "Conexão efetuada com sucesso";
     }
+
+    $sql = "SELECT * FROM 'tarefas'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0){
+        echo "Número de registros retornados: " . $result->num_rows;
+
+    }else{
+        echo " Ele não retornou nenhum registro!";
+    
+    }
+    $conn->close();
